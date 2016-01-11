aab28d941e72d3bf524ac9419fab25529901d39f
Chris w <chrislbw@hotmail.co.uk>
2016-01-11 06:05:49 +0000

    Added Textures to some areas Health Finished and working

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickup : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            col.gameObject.SendMessage("TakeDamage", -0.2f);
+
+            Destroy(gameObject);
+        }
+    }
+
+}
