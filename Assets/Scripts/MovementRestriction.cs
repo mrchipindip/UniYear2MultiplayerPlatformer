8f7bc044f12fb50b068d97ac7d069afaa13f3849
Chris w <chrislbw@hotmail.co.uk>
2016-01-14 14:58:02 +0000

    Added Some buttons to tutorial Scene
    Added Restriction to Player 1 to prevent moving when against an object or player
    -Needs to be implemented on Player 2
    -Several new sorting layers were added to accommodate the feature

---
+++ Assets/Scripts/MovementRestriction.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementRestriction : MonoBehaviour {
+
+    public GameObject Player;
+
+    private PLayerMovement2 playerMovement;
+
+    void Start () {
+        playerMovement = Player.GetComponent<PLayerMovement2>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter()
+    {
+        playerMovement.againstObject = true;
+    }
+
+    void OnTriggerExit()
+    {
+        playerMovement.againstObject = false;
+    }
+}
--- Assets/Scripts/PLayerMovement2.cs
+++ Assets/Scripts/PLayerMovement2.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PLayerMovement2 : MonoBehaviour {
-
+    
     public float moveSpeed = 10.0f;
     public float healthAmount = 1.0f;
     public float lookSpeed = 4.0f;
@@ -14,6 +14,10 @@ public class PLayerMovement2 : MonoBehaviour {
     public float jumpHeight = 8.0f;
     public Rigidbody rb;
     private float translation = 0.0f;
+
+    public bool againstObject = false;
+    private bool canMoveLeft = true;
+    private bool canMoveRight = true;
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
@@ -24,13 +28,15 @@ public class PLayerMovement2 : MonoBehaviour {
 
         // transform.position += transform.forward * Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         //transform.position += transform.right * Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        if (Input.GetAxis("Horizontal") > 0)
+        if (Input.GetAxis("Horizontal") > 0 && canMoveRight == true)
         {
+            Debug.Log("Can Move right :" + canMoveRight);
             translation = Input.GetAxis("Horizontal") * moveSpeed * -1;
             transform.Translate(0, 0, translation);
         }
-        else if (Input.GetAxis("Horizontal") < 0)
+        else if (Input.GetAxis("Horizontal") < 0 && canMoveLeft == true)
         {
+            Debug.Log("Can Move right :" + canMoveRight);
             translation = Input.GetAxis("Horizontal") * moveSpeed;
             transform.Translate(0, 0, translation);
         }
@@ -54,6 +60,8 @@ public class PLayerMovement2 : MonoBehaviour {
 
     void FixedUpdate()
     {
+        CheckIfAgainstObject();
+
         if (Input.GetAxis("Horizontal") > 0)
         {
             if (transform.rotation.eulerAngles.y != -90)
@@ -73,7 +81,7 @@ public class PLayerMovement2 : MonoBehaviour {
     void OnCollisionStay()
     {
         isFalling = false;
-        Debug.Log("CollsionStayyyyy");
+       // Debug.Log("CollsionStayyyyy");
     }
 
     void TakeDamage(float damage)
@@ -99,4 +107,29 @@ public class PLayerMovement2 : MonoBehaviour {
         yield return new WaitForSeconds(0.5f);
         stayCheckOne = false;
     }
+
+    void CheckIfAgainstObject()
+    {
+        if (againstObject == true)
+        {
+            //Debug.Log("Is against an object");
+
+            if (transform.rotation.eulerAngles.y >= 89 && transform.rotation.eulerAngles.y <= 91)
+            {
+                canMoveLeft = false;
+                Debug.Log("can move left changed");
+            }
+            else
+            {
+                canMoveRight = false;
+               // Debug.Log("can move right changed");
+            }
+        }
+        else
+        {
+            canMoveLeft = true;
+            canMoveRight = true;
+        }
+    }
+
 }
