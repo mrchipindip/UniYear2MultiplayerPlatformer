cde57ae4363d0c54f0cdc5d564f1cb38dab3e70b
Chris w <chrislbw@hotmail.co.uk>
2016-01-15 02:40:24 +0000

    Finished layout and functionality for Level 1 -Added ability for buttons and triggers to enable game objects
    Also removed unused scenes

---
+++ Assets/Scripts/ActivateObject.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateObject : MonoBehaviour {
+    public GameObject itemToEnable;
+    public GameObject itemToEnable2;
+    public GameObject itemToEnable3;
+	
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            RunActivate();
+        }
+    }
+
+    void RunActivate()
+    {
+        itemToEnable.SetActive(true);
+
+        if (itemToEnable2 != null)
+        {
+            itemToEnable2.SetActive(true);
+        }
+
+        if (itemToEnable3 != null)
+        {
+            itemToEnable3.SetActive(true);
+        }
+    }
+}
