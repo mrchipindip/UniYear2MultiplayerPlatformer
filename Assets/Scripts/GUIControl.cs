6b9bc8c0445d4f155f8b2e52327b22ea98c35426
Chris w <chrislbw@hotmail.co.uk>
2016-01-17 12:53:17 +0000

    Added Both Character Textures and created new prefabs for completed characters -Changed Block script slightly so that the arm is also included in the hide and show -Made some slight changes to Level1 layout (added more planes to not show the bottom of world to stop sides being visible at some times

---
+++ Assets/Scripts/GUIControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIControl : MonoBehaviour {
+
+	private GameObject controls;
+
+	void Start(){
+		controls = GameObject.Find ("PanelControls");
+		controls.SetActive(false);
+	}
+	//loads the scene with the index that is passed to it
+	public void ChangeToScene (int sceneToChangeTo) {
+		Application.LoadLevel (sceneToChangeTo);
+	}
+
+	public void QuitGame() {
+		Application.Quit ();
+	}
+
+	public void EnableGameobject(){
+
+		if (controls.activeInHierarchy == true)
+		{
+			controls.SetActive(false);
+		} else if (controls.activeInHierarchy == false) {
+			controls.SetActive(true);
+		}
+
+	}
+
+	public void ToggleMute(){
+		//AudioListener is outputing sounds at volume, mute. if not unmute
+		if(AudioListener.volume == 1){
+			AudioListener.volume = 0;
+		}else {
+			AudioListener.volume = 1;
+		}
+	}
+}
--- Assets/Scripts/Special Abilities/Block.cs
+++ Assets/Scripts/Special Abilities/Block.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Block : MonoBehaviour {
 
     public GameObject shieldObj;
+    public GameObject shieldObj2;
 	// Use this for initialization
 	void Start () {
 	
@@ -14,10 +15,12 @@ public class Block : MonoBehaviour {
 	    if(Input.GetAxis("Block") != 0)
         {
             shieldObj.SetActive(true);
+            shieldObj2.SetActive(true);
         }
         if (Input.GetAxis("Block") == 0 && shieldObj.activeInHierarchy == true)
         {
             shieldObj.SetActive(false);
+            shieldObj2.SetActive(false);
             //code changed
         }
     }
